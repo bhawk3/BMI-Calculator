fea399770fb089b62625f26fda99fc71a807f066
Brendan Hawk <brendanhawk03@aol.com>
2024-12-06 14:24:47 -0500

    Basic JS outline is completed
    Determined the necessary formula and set up function to run on click of the button. Set the inputs from HTML to var in JS.

---
+++ index.js
@@ -0,0 +1,30 @@
+/* Formula: BMI = weight (pounds) / [height (in)]2 x 703
+
+For example, if someone weighs 180 lbs and is 5'5" (65 in total), their BMI would be calculated as:
+180 x 703 = 126,540
+126,540 / 65 = 1,946.769
+1,946.769 / 65 = 29.95%
+
+*/
+
+
+// Collect user input on click
+// Convert heightFeet from feet to inches (Multiply by 12)
+// Add heightFeet and heightInches and set to a new variable called combinedHeight
+// Multiply weight by 703
+// Divide product of the above step and divide it by combinedHeight
+// Divide the quotient of the above step again by combinedHeight to receive BMI
+// Display BMI to the user
+
+
+let heightFeet = document.getElementsById("height-feet")
+let heightInches = document.getElementById("height-inches")
+let weight = document.getElementById("weight-pounds")
+
+
+function calculateBMI() {
+
+
+
+
+}
